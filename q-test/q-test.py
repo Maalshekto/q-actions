32cd59652a5f3f2437e2480c9d6581aee1531ae0
Thomas MARTIN <tmartin@live.fr>
2024-06-09 17:29:00 +0000

    Change to simulator

--- q-test/q-test.py
+++ q-test/q-test.py
@@ -1,6 +1,8 @@
 import numpy as np
 from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2 as Sampler
 from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import AerSimulator
 import os
 
 res = os.getenv("QISKIT_IBM_TOKEN")
@@ -17,10 +19,19 @@ from qiskit_ibm_runtime import QiskitRuntimeService
 example_circuit = QuantumCircuit(2)
 example_circuit.measure_all()
  
-backend = service.least_busy(operational=True, simulator=False)
+# backend = service.least_busy(operational=True, simulator=False)
+
+# Transpile for simulator
+simulator = AerSimulator()
+example_circuit = transpile(example_circuit, simulator)
+
+result = simulator.run(example_circuit).result()
+counts = result.get_counts(example_circuit)
+
+print(counts)
  
-sampler = Sampler(backend)
-job = sampler.run([example_circuit])
-print(f"job id: {job.job_id()}")
-result = job.result()
-print(result)
+#sampler = Sampler(backend)
+#job = sampler.run([example_circuit])
+#print(f"job id: {job.job_id()}")
+#result = job.result()
+#print(result)
